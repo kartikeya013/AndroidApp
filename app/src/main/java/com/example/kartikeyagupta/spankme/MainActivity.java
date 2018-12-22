c4418264e4b783e0a34987b8fa0d371d5195b282
Kartikeya Gupta <kartikeyagupta2000@gmail.com>
2018-12-23 03:41:41 +0530

    final touches remaining

--- app/src/main/java/com/example/kartikeyagupta/spankme/MainActivity.java
+++ app/src/main/java/com/example/kartikeyagupta/spankme/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.kartikeyagupta.spankme;
-
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     int count = 0;
-    Button button1;
     TextView abc ;
     EditText inp ;
     @Override
@@ -20,23 +21,27 @@ public class MainActivity extends AppCompatActivity {
         abc = (TextView) findViewById(R.id.textView2);
         inp =  (EditText) findViewById(R.id.input)  ;  }
 
-
-
-    public void addone (View v) {
+       public void addone (View v) {
 
         count++;
 
-        abc.setText(Integer.toString(count));
+
+          abc.setText(Integer.toString(count));
+          abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
     }
     public void subtractone (View v) {
 
         count--;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }
     public void reset (View v) {
 
         count=0;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }
     public void manualadd (View v) {
         int a ;
@@ -44,22 +49,32 @@ public class MainActivity extends AppCompatActivity {
         a= Integer.parseInt(b);
         count=count+a;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }
     public void subtractfive (View v) {
 
         count=count-5;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }public void subtractten (View v) {
 
         count=count-10;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }public void addfive (View v) {
 
         count=count+5;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }public void addten (View v) {
 
         count=count+10;
         abc.setText(Integer.toString(count));
+        abc.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+
     }
 }
