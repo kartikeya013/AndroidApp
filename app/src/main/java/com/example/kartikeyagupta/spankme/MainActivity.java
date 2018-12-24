68807c227211e68e56eeeb2d8779eb0efa99bd73
Kartikeya Gupta <kartikeyagupta2000@gmail.com>
2018-12-24 18:22:53 +0530

    lol

--- app/src/main/java/com/example/kartikeyagupta/spankme/MainActivity.java
+++ app/src/main/java/com/example/kartikeyagupta/spankme/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.kartikeyagupta.spankme;
-import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
-import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
